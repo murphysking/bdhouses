a599513f45630a37c14a872b68832fc9b7c4cc0a
murphysking <murphysking@gmail.com>
2025-01-09 10:41:42 +0800

    增加乌鲁基塔、奥迪尔利塔、德利勘、晨曦之国、永冬之山房屋信息

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+fork自[https://github.com/jsnimda/bdhouses](https://github.com/jsnimda/bdhouses),增加乌鲁基塔、奥迪尔利塔、德利勘、晨曦之国、永冬之山房屋信息
+
+
 # bdhouses
 https://jsnimda.github.io/bdhouses/
 
--- search.vue
+++ search.vue
@@ -2,12 +2,12 @@
 <div class="ui floating message">
   <i class="close icon" @click="$emit('close')"></i>
   <div class="header">
-    製作搜索：{{ title }} ( {{matches.length}} )
+    制作搜索：{{ title }} ( {{matches.length}} )
   </div>
   <div class="ui divider"></div>
   <div>
     <sorttable class="ui sortable celled table" ref="table" v-if="matches.length !== 0" :data="matches"
-      head="名稱 用途 等級 最高等級 貢獻">
+      head="名称 用途 等级 最高等级 贡献">
     </sorttable>
     <ul class="list" v-else>
       <li>No result.</li>
